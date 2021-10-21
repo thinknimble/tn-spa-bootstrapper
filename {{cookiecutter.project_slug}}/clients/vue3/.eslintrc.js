ed10ed48e1a606ccd8ce27b365a2a069b1eb6355
Pari Baker <pari@thinknimble.com>
2021-10-21 08:51:46 -0700

    rules

--- {{cookiecutter.project_slug}}/clients/vue3/.eslintrc.js
+++ {{cookiecutter.project_slug}}/clients/vue3/.eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-unused-vars': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-components': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-unused-components': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-v-model-argument': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
   overrides: [
     {
