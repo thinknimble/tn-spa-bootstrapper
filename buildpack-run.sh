961fcb198c126e30a8b338c1b57d3d44bdbb1643
Ed Romano <oudeis.metis@gmail.com>
2022-02-02 16:18:23 -0500

    Switch to heroku-18 which should have cookiecutter

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -5,5 +5,5 @@ ls
 # apt-get --fix-missing update
 # apt-get install cookiecutter
 # pip3 install cookiecutter
-python -m pip install --user cookiecutter
+# python -m pip install --user cookiecutter
 cookiecutter --help
