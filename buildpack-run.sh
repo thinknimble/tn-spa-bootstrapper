6a37a358cacb2ae9aa01561c8b70ec6b03d25811
Ed Romano <oudeis.metis@gmail.com>
2022-02-02 16:12:03 -0500

    try the other way to pip install

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -2,7 +2,8 @@
 echo "Hello World"
 pwd
 ls
-apt-get --fix-missing update
-apt-get install cookiecutter
+# apt-get --fix-missing update
+# apt-get install cookiecutter
 # pip3 install cookiecutter
+python3 -m pip install --user cookiecutter
 cookiecutter --help
