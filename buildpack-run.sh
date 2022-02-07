081ce495209066ff051f0488c61d1e0096204c78
Ed Romano <oudeis.metis@gmail.com>
2022-02-07 18:51:43 -0500

    that works. See if we can promote that folder to root

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-echo "Hello World"
-pwd
-ls
-# apt-get --fix-missing update
-# apt-get install cookiecutter
-# pip3 install cookiecutter
-# python -m pip install --user cookiecutter
-cookiecutter --no-input
-# apt list | grep cookie
+echo "Running cookiecutter on the current directory; accepting all defaults"
+cookiecutter --no-input .
