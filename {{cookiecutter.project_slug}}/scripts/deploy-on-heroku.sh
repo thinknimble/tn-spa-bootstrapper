cb32a754983d6988b56e833200a3b4b2c016f57b
danielnazarian <daniel@thinknimble.com>
2022-03-07 15:40:47 -0500

    added basic config to 'deploy-on-heroku'

--- {{cookiecutter.project_slug}}/scripts/deploy-on-heroku.sh
+++ {{cookiecutter.project_slug}}/scripts/deploy-on-heroku.sh
@@ -23,6 +23,8 @@ heroku config:set DEBUG="True" --app $APP_NAME
 heroku config:set CURRENT_DOMAIN="$APP_NAME.herokuapp.com" --app $APP_NAME
 heroku config:set ALLOWED_HOSTS="$APP_NAME.herokuapp.com,localhost" --app $APP_NAME
 heroku config:set NPM_CONFIG_PRODUCTION=false --app $APP_NAME
+heroku config:set USE_ROLLBAR=True --app $APP_NAME
+heroku config:set ROLLBAR_ACCESS_TOKEN="TODO" --app $APP_NAME
 git init 
 heroku git:remote --app $APP_NAME
 git branch -M main
