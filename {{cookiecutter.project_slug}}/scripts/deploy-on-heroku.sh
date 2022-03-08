68ecd8f0a9ed52b38b4884758826ee43324fd3d1
danielnazarian <daniel@thinknimble.com>
2022-03-08 12:13:53 -0500

    updated deploy script

--- {{cookiecutter.project_slug}}/scripts/deploy-on-heroku.sh
+++ {{cookiecutter.project_slug}}/scripts/deploy-on-heroku.sh
@@ -9,6 +9,11 @@ heroku create $APP_NAME --buildpack heroku/python
 heroku addons:create heroku-postgresql:hobby-dev --app $APP_NAME
 {%- if cookiecutter.client_app.lower() != "none" %}
 heroku buildpacks:add --index 1 heroku/nodejs --app $APP_NAME
+
+echo "Please fill in your ROLLBAR_ACCESS_TOKEN"
+read -p 'ROLLBAR_ACCESS_TOKEN: ' ROLLBAR_ACCESS_TOKEN
+
+
 if [ -z "${NPM_PRIVATE_TOKEN}" ]; then
     echo "Please fill in your NPM_PRIVATE_TOKEN"
     read -p 'NPM_PRIVATE_TOKEN: ' NPM_PRIVATE_TOKEN
@@ -17,18 +22,16 @@ else
     NPM_PRIVATE_TOKEN = "${NPM_PRIVATE_TOKEN}"
     heroku config:set NPM_PRIVATE_TOKEN=$NPM_PRIVATE_TOKEN --app $APP_NAME
 fi
-if [ -z "${ROLLBAR_ACCESS_TOKEN}" ]; then
-    heroku config:set USE_ROLLBAR="False" --app $APP_NAME
-else
-    heroku config:set USE_ROLLBAR="True" --app $APP_NAME
-    heroku config:set ROLLBAR_ACCESS_TOKEN="${ROLLBAR_ACCSESS_TOKEN}" --app $APP_NAME
-fi
+
+
 {%- endif %}
 heroku config:set SECRET_KEY="$(openssl rand -base64 64)" --app $APP_NAME
 heroku config:set DEBUG="True" --app $APP_NAME
 heroku config:set CURRENT_DOMAIN="$APP_NAME.herokuapp.com" --app $APP_NAME
 heroku config:set ALLOWED_HOSTS="$APP_NAME.herokuapp.com,localhost" --app $APP_NAME
 heroku config:set NPM_CONFIG_PRODUCTION=false --app $APP_NAME
+heroku config:set ROLLBAR_ACCESS_TOKEN="$ROLLBAR_ACCSESS_TOKEN" --app $APP_NAME
+fi
 git init 
 heroku git:remote --app $APP_NAME
 git branch -M main
