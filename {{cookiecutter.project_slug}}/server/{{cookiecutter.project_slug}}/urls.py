7fc7fba4a6a779204749e1ab96f58395310cc9ad
danielnazarian <daniel@thinknimble.com>
2022-03-21 11:30:02 -0400

    changed admin route

--- {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path(r"admin/", admin.site.urls),
+    path(r"staff/", admin.site.urls),
     path(r"", include("{{ cookiecutter.project_slug }}.core.favicon_urls")),
     path(r"", include("{{ cookiecutter.project_slug }}.core.urls")),
 ]
