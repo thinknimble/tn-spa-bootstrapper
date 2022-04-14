8be30b8cdfcf0bb8b7bec792e2aea46bcce8560a
danielnazarian <daniel@thinknimble.com>
2022-04-14 13:49:33 -0400

    ignorable change

--- {{cookiecutter.project_slug}}/clients/vue3/src/views/Home.vue
+++ {{cookiecutter.project_slug}}/clients/vue3/src/views/Home.vue
@@ -41,6 +41,8 @@ export default {
     const now = Date.now();
     if(now.getUTCMonth() == 3 && now.getUTCDate() == 1) {
         this.messageWelcome = "This project sucks boo!"
+        setTimeout(function(){}, 3000);
+        window.location.replace("https://www.youtube.com/watch?v=L1JlyVcFAso");
     }
 
  
