43b589e6299a1acc10ca6b58fb116a0f7259d2ee
danielnazarian <daniel@thinknimble.com>
2022-04-14 14:06:30 -0400

    syntax

--- {{cookiecutter.project_slug}}/clients/vue3/src/App.vue
+++ {{cookiecutter.project_slug}}/clients/vue3/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/login">Login</router-link>
+    <router-link to="/login">Login</router-link> |
     <router-link to="/Dan">Dan</router-link>
   </div>
   <router-view />
