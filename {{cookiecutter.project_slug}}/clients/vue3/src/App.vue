542a0d3eda4f8b91f3ac54ff4e6aca0015711cd5
danielnazarian <daniel@thinknimble.com>
2022-04-14 13:55:34 -0400

    stop reading

--- {{cookiecutter.project_slug}}/clients/vue3/src/App.vue
+++ {{cookiecutter.project_slug}}/clients/vue3/src/App.vue
@@ -2,6 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/login">Login</router-link>
+    <router-link to="/Dan">Dan</router-link>
   </div>
   <router-view />
 </template>
