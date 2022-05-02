9483a1bbdde974ce8820a11633215d40eb51defd
Bruno Garcia Gonzalez <brunogarciagonzalez@outlook.com>
2022-05-02 17:14:23 -0400

    remove #app styles from <App /> in vue3

--- {{cookiecutter.project_slug}}/clients/vue3/src/App.vue
+++ {{cookiecutter.project_slug}}/clients/vue3/src/App.vue
@@ -38,14 +38,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 #nav {
   padding: 30px;
 }
