ca500a1e5c8445ca6db44c4785274209af55e6fd
Bruno Garcia Gonzalez <brunogarciagonzalez@outlook.com>
2022-05-02 16:47:58 -0400

    added bulma (ui) and mq (breakpoints) to vue3 client

--- {{cookiecutter.project_slug}}/clients/vue3/src/main.js
+++ {{cookiecutter.project_slug}}/clients/vue3/src/main.js
@@ -1,6 +1,23 @@
+require('@/styles/main.scss')
 import { createApp } from 'vue'
+import { Vue3Mq } from 'vue3-mq'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// same breakpoints as Bulma standard config
+const breakpoints = {
+  mobile: 0,
+  tablet: 769,
+  desktop: 1024,
+  widescreen: 1216,
+  fullhd: 1408,
+}
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(Vue3Mq, {
+    breakpoints,
+  })
+  .mount('#app')
