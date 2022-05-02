7f8d9257c62b3e563528de68cbd2d70578761027
Bruno Garcia Gonzalez <brunogarciagonzalez@outlook.com>
2022-05-02 17:28:52 -0400

    added bulma to vue3 <Login />

--- {{cookiecutter.project_slug}}/clients/vue3/src/views/Login.vue
+++ {{cookiecutter.project_slug}}/clients/vue3/src/views/Login.vue
@@ -1,33 +1,54 @@
 <template>
-  <div class="login">
-    <form @submit.prevent="attemptLogin()">
-      <InputField
-        v-model:value="form.email.value"
-        :errors="form.email.errors"
-        @blur="form.email.validate()"
-        type="email"
-        label="Email:"
-        placeholder="Email"
-      />
+  <div class="container is-max-desktop section" :class="{ box: isDesktopPlus }">
+    <h1 class="title">Login.</h1>
+    <h2 class="subtitle">Welcome back! Login with the data you entered during registration.</h2>
+    <div class="columns is-centered">
+      <div class="column">
+        <form class="mb-4" @submit.prevent="attemptLogin()">
+          <InputField
+            v-model:value="form.email.value"
+            :errors="form.email.errors"
+            @blur="form.email.validate()"
+            type="email"
+            label="Email:"
+            placeholder="Enter your email..."
+          />
 
-      <InputField
-        v-model:value="form.password.value"
-        :errors="form.password.errors"
-        @blur="form.password.validate()"
-        type="password"
-        label="Password:"
-        placeholder="Password"
-      />
+          <InputField
+            v-model:value="form.password.value"
+            :errors="form.password.errors"
+            @blur="form.password.validate()"
+            type="password"
+            label="Password:"
+            placeholder="Enter your password..."
+          />
 
-      <button type="submit">Login</button>
-    </form>
+          <button class="button is-fullwidth is-primary" type="submit">Login</button>
+        </form>
+        <div class="level mb-0">
+          <div class="level-item has-text-centered">
+            <p>Don't have an account?</p>
+            <button class="button is-ghost" @click.prevent="routeToSignup()">Sign up</button>
+          </div>
+        </div>
+        <div class="level">
+          <div class="level-item has-text-centered">
+            <button class="button is-ghost" @click.prevent="routeToRequestPasswordReset()">
+              Forgot password?
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
+import { useMq } from 'vue3-mq'
+
 import User, { LoginForm } from '@/services/users/'
 import InputField from '@/components/inputs/InputField'
 
@@ -39,6 +60,7 @@ export default {
   setup() {
     const store = useStore()
     const router = useRouter()
+    const mq = useMq()
     const form = ref(new LoginForm())
 
     async function handleLoginSuccess({ data }) {
@@ -70,6 +92,9 @@ export default {
     return {
       form,
       attemptLogin,
+      isDesktopPlus: computed(() => mq.desktopPlus),
+      routeToSignup: () => router.push({ name: 'Signup' }),
+      routeToRequestPasswordReset: () => router.push({ name: 'RequestPasswordReset' }),
     }
   },
 }
