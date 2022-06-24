50cc1cc876a8c131f74c71c34b8e87bff92f2f82
Bruno Garcia Gonzalez <brunogarciagonzalez@outlook.com>
2022-06-24 11:33:01 -0400

    Added util as_choices (#91)
    * added util: as_choices()
    
    * updated docstring for utils.as_choices
    
    * added new-line at end of utils.misc

--- {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/utils/misc.py
+++ {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/utils/misc.py
@@ -12,3 +12,14 @@ def datetime_appended_filepath(instance, filename):
     time = time.split(".")[0]  # Remove trailing tz info
     name = f"{original_name}_{time}.{extension}"
     return name
+
+
+def as_choices(iterable):
+    """
+    Takes in an iterable such as a 1D List and maps it,
+    such that each element of the original iterable is mapped
+    to be both elements of a tuple.
+
+    [A, B ...] is mapped to [(A, A), (B, B) ...]
+    """
+    return tuple((elem, elem) for elem in iterable)
