8d474dd3cc471902a351a6699017218fe8f4e52c
Bruno Garcia Gonzalez <brunogarciagonzalez@outlook.com>
2022-06-24 09:51:50 -0400

    added util: as_choices()

--- {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/utils/misc.py
+++ {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/utils/misc.py
@@ -12,3 +12,15 @@ def datetime_appended_filepath(instance, filename):
     time = time.split(".")[0]  # Remove trailing tz info
     name = f"{original_name}_{time}.{extension}"
     return name
+
+def as_choices(iterable):
+    """
+    Choice-ifies a 1-Dimensional iterable (e.g. [A, B ...]) by mapping it
+    into an iterable of exactly two items (e.g. [(A, A), (A, A) ...])
+    to use as choices for a field.
+    As per the Django ORM, the first element in each tuple is the actual
+    value to be set on the model, and the second element is the human-readable name.
+    This method takes an element of args.iterable and maps it to be both
+    the actual value to be set on the model, and the human-readable name.
+    """
+    return tuple((elem, elem) for elem in iterable)
