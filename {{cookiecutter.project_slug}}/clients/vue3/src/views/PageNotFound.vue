49ec5dc21970403ea5064ef15b68cebd28682ded
Brenna Bruening <brennabruening@gmail.com>
2022-07-11 16:21:43 -0400

    create basic 404 page PageNotFound view

---
+++ {{cookiecutter.project_slug}}/clients/vue3/src/views/PageNotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page-not-found">
+    <h1><span class="error">404</span> Page Not Found</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFound',
+  components: {},
+}
+</script>
+
+<style scoped lang="scss">
+.error {
+  color: red;
+}
+</style>
