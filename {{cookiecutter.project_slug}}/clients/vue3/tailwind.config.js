c29c7f26de8da222701f5e29432db4877af9c0dc
Brenna Bruening <brennabruening@gmail.com>
2022-07-20 17:59:24 -0400

    add colors to tailwind config theme

--- {{cookiecutter.project_slug}}/clients/vue3/tailwind.config.js
+++ {{cookiecutter.project_slug}}/clients/vue3/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#ffffff',
+        black: '#000000',
+        success: '#4faf64',
+        warning: '#f4b942',
+        error: '#d72638',
+        primary: '#042642',
+        accent: '#f16158',
+      },
+    },
   },
   plugins: [],
 }
