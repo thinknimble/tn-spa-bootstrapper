174a41d49f5005e173fddc4b87e1c8648859f39e
Ed Romano <oudeis.metis@gmail.com>
2022-07-20 16:30:45 -0400

    Make generic to bootstrapper

--- {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/utils/tests.py
+++ {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/utils/tests.py
@@ -1,6 +1,6 @@
 import pytest
 
-from rewyre.utils.sites import get_site_url
+from {{ cookiecutter.project_slug }}.utils.sites import get_site_url
 
 
 @pytest.mark.parametrize(
@@ -8,7 +8,7 @@ from rewyre.utils.sites import get_site_url
     [
         ({"CURRENT_DOMAIN": "localhost", "CURRENT_PORT": 8080, "HEROKU_APP_NAME": None, "IN_DEV": True}, "http://localhost:8080"),
         ({"CURRENT_DOMAIN": "http://localhost/", "CURRENT_PORT": None, "HEROKU_APP_NAME": "", "IN_DEV": False}, "https://localhost"),
-        ({"CURRENT_DOMAIN": "localhost", "CURRENT_PORT": 8080, "HEROKU_APP_NAME": None, "IN_DEV": True}, "http://localhost:8080"),
+        ({"CURRENT_DOMAIN": "", "CURRENT_PORT": None, "HEROKU_APP_NAME": "example", "IN_DEV": False}, "https://example.herokuapp.com"),
     ],
 )
 def test_get_site_url(settings, custom_settings, expected_output):
@@ -22,7 +22,7 @@ def test_get_site_url(settings, custom_settings, expected_output):
     "custom_settings,expected_output",
     [
         ({"CURRENT_DOMAIN": None, "CURRENT_PORT": 8080, "HEROKU_APP_NAME": None, "IN_DEV": True}, "http://localhost:8080"),
-        ({"CURRENT_DOMAIN": "", "CURRENT_PORT": None, "HEROKU_APP_NAME": "", "IN_DEV": False}, "https://rewyre.herokuapp.com"),
+        ({"CURRENT_DOMAIN": "", "CURRENT_PORT": None, "HEROKU_APP_NAME": "", "IN_DEV": False}, "https://example.herokuapp.com"),
     ],
 )
 def test_get_site_url_negative(settings, custom_settings, expected_output):
