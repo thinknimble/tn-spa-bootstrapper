35d1df2fb247a7ba6a83220efa4010e4a56cd3a6
Ed Romano <oudeis.metis@gmail.com>
2022-07-20 16:23:22 -0400

    Fixing get_site_url bugs with solid tests

---
+++ {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/utils/tests.py
@@ -0,0 +1,32 @@
+import pytest
+
+from rewyre.utils.sites import get_site_url
+
+
+@pytest.mark.parametrize(
+    "custom_settings,expected_output",
+    [
+        ({"CURRENT_DOMAIN": "localhost", "CURRENT_PORT": 8080, "HEROKU_APP_NAME": None, "IN_DEV": True}, "http://localhost:8080"),
+        ({"CURRENT_DOMAIN": "http://localhost/", "CURRENT_PORT": None, "HEROKU_APP_NAME": "", "IN_DEV": False}, "https://localhost"),
+        ({"CURRENT_DOMAIN": "localhost", "CURRENT_PORT": 8080, "HEROKU_APP_NAME": None, "IN_DEV": True}, "http://localhost:8080"),
+    ],
+)
+def test_get_site_url(settings, custom_settings, expected_output):
+    for key in custom_settings:
+        settings.__setattr__(key, custom_settings[key])
+    site_url = get_site_url()
+    assert site_url == expected_output
+
+
+@pytest.mark.parametrize(
+    "custom_settings,expected_output",
+    [
+        ({"CURRENT_DOMAIN": None, "CURRENT_PORT": 8080, "HEROKU_APP_NAME": None, "IN_DEV": True}, "http://localhost:8080"),
+        ({"CURRENT_DOMAIN": "", "CURRENT_PORT": None, "HEROKU_APP_NAME": "", "IN_DEV": False}, "https://rewyre.herokuapp.com"),
+    ],
+)
+def test_get_site_url_negative(settings, custom_settings, expected_output):
+    for key in custom_settings:
+        settings.__setattr__(key, custom_settings[key])
+    with pytest.raises(Exception):
+        get_site_url()
