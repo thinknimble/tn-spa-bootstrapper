917880762c90444d4b5b79c13efb2a235138355e
Brenna Bruening <brennabruening@gmail.com>
2022-07-21 15:49:30 -0400

    syntax cleanup

--- {{cookiecutter.project_slug}}/clients/vue3/src/App.vue
+++ {{cookiecutter.project_slug}}/clients/vue3/src/App.vue
@@ -1,31 +1,32 @@
 <template>
   <div class="app font-avenir antialiased text-center">
     <div id="nav" class="nav flex justify-center p-7">
-      <router-link to="/" class="router font-bold px-2 hover:cursor-pointer active:bg-blue-600"
-        >Home</router-link
-      >
+      <router-link to="/" class="router font-bold px-2 hover:cursor-pointer active:bg-blue-600">
+        Home
+      </router-link>
       <template v-if="isLoggedIn">
         |
-        <router-link to="/dashboard" class="router font-bold px-2 hover:cursor-pointer"
-          >Dashboard</router-link
-        >
+        <router-link to="/dashboard" class="router font-bold px-2 hover:cursor-pointer">
+          Dashboard
+        </router-link>
         |
         <span
           id="logout"
           class="router font-bold px-2 hover:cursor-pointer underline"
           @click="logout()"
-          >Logout</span
         >
+          Logout
+        </span>
       </template>
       <template v-else>
         |
-        <router-link to="/login" data-cy="login" class="router font-bold px-2 hover:cursor-pointer"
-          >Login</router-link
-        >
+        <router-link to="/login" data-cy="login" class="router font-bold px-2 hover:cursor-pointer">
+          Login
+        </router-link>
         |
-        <router-link to="/signup" class="router font-bold px-2 hover:cursor-pointer"
-          >Signup</router-link
-        >
+        <router-link to="/signup" class="router font-bold px-2 hover:cursor-pointer">
+          Signup
+        </router-link>
       </template>
     </div>
     <router-view />
