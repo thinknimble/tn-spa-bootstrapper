dfe7d53984a7a0d9b52e8cbf6e86a2212a84d2b4
Mikael Davis <mikael@thinknimble.com>
2022-08-22 12:45:25 -0400

    add base admin functionality back

--- {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/core/admin.py
+++ {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/core/admin.py
@@ -2,4 +2,51 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+# this is a required model for the admin console to hash passwords
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password",
+                    "last_login",
+                    "is_staff",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+
+    list_display = ("is_active", "email", "first_name", "last_name")
+
+    list_display_links = (
+        "is_active",
+        "email",
+        "first_name",
+        "last_name",
+    )
+
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name",
+    )
+
+    ordering = []
+
+admin.site.register(User, CustomUserAdmin)
