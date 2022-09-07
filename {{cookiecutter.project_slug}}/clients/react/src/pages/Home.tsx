070cda53726e3cf2a236bacc60374a3fedbb69b5
wes <web3wes@gmail.com>
2022-09-07 14:04:40 -0400

    add login and signout

--- {{cookiecutter.project_slug}}/clients/react/src/pages/Home.tsx
+++ {{cookiecutter.project_slug}}/clients/react/src/pages/Home.tsx
@@ -1,5 +1,27 @@
-import React from "react"
+import React, { useState, useContext } from "react"
+import { AuthContext, logout } from "src/utils/auth"
+import { Outlet, useNavigate } from "react-router-dom"
 
 export default function Home() {
-  return <h1>Homepage</h1>
+  const { updateToken } = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const logUserOut = () => {
+    logout()
+    updateToken(null)
+    navigate("/log-in")
+  }
+
+  // return <button onClick={() => logUserOut()}> Sign out</button>
+
+  return (
+    <>
+      <button
+        onClick={() => logUserOut()}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Sign out
+      </button>
+    </>
+  )
 }
--- {{cookiecutter.project_slug}}/clients/react/src/pages/LogIn.tsx
+++ {{cookiecutter.project_slug}}/clients/react/src/pages/LogIn.tsx
@@ -14,7 +14,7 @@ export default function LogIn() {
     onCompleted: (data) => {
       localStorage.setItem("auth-token", data.tokenAuth.token)
       updateToken(data.tokenAuth.token)
-      navigate("/surveys")
+      navigate("/home")
     },
     onError: (error) => {
       const errorMessage = error
@@ -42,16 +42,60 @@ export default function LogIn() {
     }
   }
   return (
-    <>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/tailwind.min.css"
-      />
-
-      <div className="grid grid-cols-5 gap-3">
-        <div className="bg-blue-100">1st col</div>
-        <div className="bg-red-100 col-span-4">2nd col</div>
+    <div className="grid h-screen place-items-center">
+      <div className="w-full max-w-xs">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              // for="username"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="Email"
+              type="text"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              // for="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <p className="text-red-500 text-xs italic">
+              Please choose a password.
+            </p>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              onClick={handleLogin}
+            >
+              Log In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+        <p className="text-center text-gray-500 text-xs"></p>
       </div>
-    </>
+    </div>
   )
 }
