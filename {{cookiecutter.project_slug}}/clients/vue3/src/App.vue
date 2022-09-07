5bc1087e423ccf56a1ea8ee85f7303d82e242dd2
Brenna Bruening <brennabruening@gmail.com>
2022-09-07 13:29:25 -0400

    add global class styling for router class

--- {{cookiecutter.project_slug}}/clients/vue3/src/App.vue
+++ {{cookiecutter.project_slug}}/clients/vue3/src/App.vue
@@ -1,32 +1,18 @@
 <template>
   <div class="font-avenir antialiased text-center">
     <div class="flex justify-center p-7">
-      <router-link to="/" class="router font-bold px-2 hover:cursor-pointer active:bg-blue-600">
-        Home
-      </router-link>
+      <router-link to="/" class="router">Home</router-link>
       <template v-if="isLoggedIn">
         |
-        <router-link to="/dashboard" class="router font-bold px-2 hover:cursor-pointer">
-          Dashboard
-        </router-link>
+        <router-link to="/dashboard" class="router">Dashboard</router-link>
         |
-        <span
-          id="logout"
-          class="router font-bold px-2 hover:cursor-pointer underline"
-          @click="logout()"
-        >
-          Logout
-        </span>
+        <span id="logout" class="router underline" @click="logout()">Logout</span>
       </template>
       <template v-else>
         |
-        <router-link to="/login" data-cy="login" class="router font-bold px-2 hover:cursor-pointer">
-          Login
-        </router-link>
+        <router-link to="/login" data-cy="login" class="router">Login</router-link>
         |
-        <router-link to="/signup" class="router font-bold px-2 hover:cursor-pointer">
-          Signup
-        </router-link>
+        <router-link to="/signup" class="router">Signup</router-link>
       </template>
     </div>
     <router-view />
