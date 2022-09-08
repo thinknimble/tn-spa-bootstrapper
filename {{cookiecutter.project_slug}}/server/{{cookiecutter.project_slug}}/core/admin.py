8dba5047464d8caa59a7def6d64677bf8573c2be
thin-paribaker <pari@thinknimble.com>
2022-09-08 08:43:32 -0700

    fix isort

--- {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/core/admin.py
+++ {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .models import User
 
