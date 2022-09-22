945eb69d0d9abdbb00b131379e373c3300e05df6
Ed Romano <oudeis.metis@gmail.com>
2022-09-22 10:41:30 -0400

    fetch main so it's available

--- scripts/vue_or_react.sh
+++ scripts/vue_or_react.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # cookiecutter tn-spa-bootstrapper --config-file tn-spa-bootstrapper/cookiecutter/vue_template.yaml --no-input
+git fetch origin main
+git diff --name-only main
 ts_count=$(git diff --name-only main | grep "\.ts$" | wc -l)
 tsx_count=$(git diff --name-only main | grep "\.tsx$" | wc -l)
 react_count=$(($ts_count + $tsx_count))
