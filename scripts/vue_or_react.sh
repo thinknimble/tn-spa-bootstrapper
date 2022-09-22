b41d94fe58ad72e7feca44d6214b54c21fe682c8
Ed Romano <oudeis.metis@gmail.com>
2022-09-22 10:46:35 -0400

    more explicit about what we are diffing

--- scripts/vue_or_react.sh
+++ scripts/vue_or_react.sh
@@ -2,11 +2,11 @@
 
 # cookiecutter tn-spa-bootstrapper --config-file tn-spa-bootstrapper/cookiecutter/vue_template.yaml --no-input
 git fetch origin main
-git diff --name-only main
-ts_count=$(git diff --name-only main | grep "\.ts$" | wc -l)
-tsx_count=$(git diff --name-only main | grep "\.tsx$" | wc -l)
+git diff --name-only origin/main --
+ts_count=$(git diff --name-only origin/main -- | grep "\.ts$" | wc -l)
+tsx_count=$(git diff --name-only origin/main -- | grep "\.tsx$" | wc -l)
 react_count=$(($ts_count + $tsx_count))
-vue_count=$(git diff --name-only main | grep "\.vue$" | wc -l)
+vue_count=$(git diff --name-only origin/main -- | grep "\.vue$" | wc -l)
 
 if [ $react_count -gt $vue_count ]; then
   echo "react"
