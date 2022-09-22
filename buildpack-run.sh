cb57e26f0b508ae44bed16f21ac5ae2cedba69b5
Ed Romano <oudeis.metis@gmail.com>
2022-09-22 11:39:40 -0400

    installs dont carry over. Heroku cant run git commands

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Running cookiecutter on the current directory; accepting all defaults"
+git init
 config_file=$(./scripts/vue_or_react.sh)
 cookiecutter . --config-file $config_file --no-input -f
