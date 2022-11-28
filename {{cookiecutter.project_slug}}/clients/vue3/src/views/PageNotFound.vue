cecf7650a0ff36a8f369c9834f0f4b073a4100c4
Brenna Bruening <brennabruening@gmail.com>
2022-11-28 17:09:13 -0500

    update PageNotFound html classes to Tailwind vs SCSS

--- {{cookiecutter.project_slug}}/clients/vue3/src/views/PageNotFound.vue
+++ {{cookiecutter.project_slug}}/clients/vue3/src/views/PageNotFound.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="page-not-found">
-    <h1 class="page-not-found__error">404</h1>
+  <div class="flex flex-col items-center">
+    <h1 class="text-7xl font-extrabold">404</h1>
     <img alt="Project Logo" src="../assets/logo.png" />
-    <h2 class="page-not-found__header">Oops!<br />We couldn't find the page you're looking for.</h2>
-    <p class="page-not-found__route">
+    <h2 class="text-xl font-bold mb-4">Oops!<br />We couldn't find the page you're looking for.</h2>
+    <p class="text-xl underline">
       Click <router-link to="/">here</router-link> to return to the home page.
     </p>
   </div>
