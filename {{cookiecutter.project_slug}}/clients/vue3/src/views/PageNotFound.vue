2b6a41ed01e297e2c27b25a9437cc9197b86dc97
Brenna Bruening <brennabruening@gmail.com>
2022-11-28 17:11:17 -0500

    update tone for 'couldn't find the page' text

--- {{cookiecutter.project_slug}}/clients/vue3/src/views/PageNotFound.vue
+++ {{cookiecutter.project_slug}}/clients/vue3/src/views/PageNotFound.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col items-center">
     <h1 class="text-7xl font-extrabold">404</h1>
     <img alt="Project Logo" src="../assets/logo.png" />
-    <h2 class="text-xl font-bold mb-4">Oops!<br />We couldn't find the page you're looking for.</h2>
+    <h2 class="text-xl font-bold mb-4">We couldn't find the page you're looking for.</h2>
     <router-link to="/" class="text-xl underline">Return to the home page.</router-link>
   </div>
 </template>
