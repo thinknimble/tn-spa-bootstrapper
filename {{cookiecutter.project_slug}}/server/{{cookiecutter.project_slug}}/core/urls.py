e3a595cabdea62a3b64ff0c7f39453b51a396748
Ed Romano <oudeis.metis@gmail.com>
2022-11-28 15:07:31 -0500

    undo change

--- {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/core/urls.py
+++ {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/core/urls.py
@@ -38,8 +38,8 @@ schema_view = get_schema_view(
         contact=openapi.Contact(email="support@{{ cookiecutter.project_slug }}.com"),
         license=openapi.License(name="BSD License"),
     ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
+    public=False,
+    permission_classes=[permissions.IsAuthenticated],
 )
 
 urlpatterns = urlpatterns + [
