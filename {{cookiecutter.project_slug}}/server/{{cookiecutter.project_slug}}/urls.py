4197058db8a941235135b58f051100b707edd238
paribaker <58012003+paribaker@users.noreply.github.com>
2022-12-19 13:50:27 -0500

    added new views (#142)

--- {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=settings.DEBUG))),
     {% endif -%}
     path(r"staff/", admin.site.urls),
-    path(r"", include("{{ cookiecutter.project_slug }}.core.favicon_urls")),
-    path(r"", include("{{ cookiecutter.project_slug }}.core.urls")),
+    path(r"", include("{{ cookiecutter.project_slug }}.common.favicon_urls")),
+    path(r"", include("{{ cookiecutter.project_slug }}.common.urls")),
 ]
