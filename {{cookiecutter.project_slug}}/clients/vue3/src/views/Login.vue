fd701be0fb425647986aca0211ba587ffa825f61
Brenna Bruening <brennabruening@gmail.com>
2023-01-26 18:30:39 -0500

    remove duplicate type on login button

--- {{cookiecutter.project_slug}}/clients/vue3/src/views/Login.vue
+++ {{cookiecutter.project_slug}}/clients/vue3/src/views/Login.vue
@@ -20,7 +20,7 @@
         label="Password"
         placeholder="Enter password..."
       />
-      <button type="button" class="btn--primary" data-cy="submit" type="submit">Login</button>
+      <button class="btn--primary" data-cy="submit" type="submit">Login</button>
       <button
         type="button"
         class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
