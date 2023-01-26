d49341d4557ae6eda72d66e371a068a494d0159d
Brenna Bruening <brennabruening@gmail.com>
2023-01-26 18:38:44 -0500

    test bg-violet-300 on test signup button to check for tailwind v3 successful integration

--- {{cookiecutter.project_slug}}/clients/vue3/src/views/Login.vue
+++ {{cookiecutter.project_slug}}/clients/vue3/src/views/Login.vue
@@ -22,13 +22,7 @@
       />
       <button class="btn--primary" data-cy="submit" type="submit">Login</button>
       <button
-        type="button"
-        class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-      >
-        Test Button
-      </button>
-      <button
-        class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        class="group relative flex w-full justify-center rounded-md border border-transparent bg-violet-300 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
       >
         <span class="absolute inset-y-0 left-0 flex items-center pl-3">
           <!-- Heroicon name: mini/lock-closed -->
