b58f5515e1ebbdc734da39e8a5c63d5ebd9e9aac
Brenna Bruening <brennabruening@gmail.com>
2023-01-26 18:29:09 -0500

    add test button #2 on login page w/ heroicon

--- {{cookiecutter.project_slug}}/clients/vue3/src/views/Login.vue
+++ {{cookiecutter.project_slug}}/clients/vue3/src/views/Login.vue
@@ -27,6 +27,27 @@
       >
         Test Button
       </button>
+      <button
+        class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      >
+        <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+          <!-- Heroicon name: mini/lock-closed -->
+          <svg
+            class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </span>
+        Sign in
+      </button>
       <div class="flex self-center m-4">
         <p class="mr-2">Don't have an account?</p>
         <router-link to="/signup" class="font-bold text-primary hover:underline">
