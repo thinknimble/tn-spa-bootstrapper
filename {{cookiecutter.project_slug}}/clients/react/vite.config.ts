260e588a4d5573a83ec855c1ec88262269bd5e92
lakardion <lakardion@protonmail.com>
2023-02-03 09:59:34 -0300

    chore: add proxy for requests in vite config

--- {{cookiecutter.project_slug}}/clients/react/vite.config.ts
+++ {{cookiecutter.project_slug}}/clients/react/vite.config.ts
@@ -1,16 +1,28 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setup-tests.ts',
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react(), tsconfigPaths()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/setup-tests.ts',
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: (env.VITE_DEV_BACKEND_URL || 'http://server:8000') + '/api',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
