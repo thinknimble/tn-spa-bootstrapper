3ed93789032836c7989881e9998ddfc37180a793
thin-paribaker <pari@thinknimble.com>
2023-02-06 21:59:07 -0800

    views fix

--- {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/common/views.py
+++ {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/common/views.py
@@ -1,8 +1,7 @@
-{% if cookiecutter.client_app == "Vue3" -%}
+{% if cookiecutter.use_graphql == "n" -%}
 from django.shortcuts import render
 from django.template.exceptions import TemplateDoesNotExist
-{% endif %}
-{% if cookiecutter.use_graphql == 'y' -%}
+{% else -%}
 from django.template.response import TemplateResponse
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import ensure_csrf_cookie
@@ -16,9 +15,6 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 @never_cache
 def index(request):
     return TemplateResponse(request, ["index.html", "core/index-placeholder.html"])
-{% elif cookiecutter.client_app.lower() == 'None' -%}
-def index(request):
-    return redirect(to="/docs/swagger/")
 {% else %}
 def index(request):
     try:
