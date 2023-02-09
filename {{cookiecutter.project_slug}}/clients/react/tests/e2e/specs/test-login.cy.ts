cd310f5953f71f50972e9edb30360635d05005cf
thin-paribaker <pari@thinknimble.com>
2023-02-08 20:36:59 -0800

    remove unnecessary scripts

--- {{cookiecutter.project_slug}}/clients/react/tests/e2e/specs/test-login.cy.ts
+++ {{cookiecutter.project_slug}}/clients/react/tests/e2e/specs/test-login.cy.ts
@@ -3,12 +3,4 @@ describe('Tests login workflow', () => {
     cy.visit('/')
     cy.url().should('include', '/log-in')
   })
-
-  it('Filling in email and passwords goes to home', () => {
-    cy.visit('/')
-    cy.get('[data-cy=email]').type(Cypress.env('TEST_USER_EMAIL'))
-    cy.get('[data-cy=password]').type(Cypress.env('TEST_USER_PASS'))
-    cy.contains('[data-cy=submit]', 'Sign In').click()
-    cy.url().should('include', '/home')
-  })
 })
