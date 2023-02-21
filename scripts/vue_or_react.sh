16c3820bea6f23d182db8f478d1213354d14b997
Ed Romano <oudeis.metis@gmail.com>
2023-02-21 13:42:18 -0500

    file path inspection for vue_or_react script

--- scripts/vue_or_react.sh
+++ scripts/vue_or_react.sh
@@ -2,13 +2,9 @@
 
 # cookiecutter tn-spa-bootstrapper --config-file tn-spa-bootstrapper/cookiecutter/vue_template.yaml --no-input
 git fetch origin main
-ts_count=$(git diff --name-only origin/main -- | grep "\.ts$" | wc -l)
-tsx_count=$(git diff --name-only origin/main -- | grep "\.tsx$" | wc -l)
-react_count=$(($ts_count + $tsx_count))
-vue_count=$(git diff --name-only origin/main -- | grep "\.vue$" | wc -l)
+react_count=$(git diff --name-only origin/main -- | grep "/clients/react/" | wc -l)
+vue_count=$(git diff --name-only origin/main -- | grep "/clients/vue/" | wc -l)
 
-react_count=100
-vue_count=0
 if [ $react_count -gt $vue_count ]; then
   echo "cookiecutter/react_template.yaml"
 else
