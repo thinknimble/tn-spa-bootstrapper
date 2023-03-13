9421388128e015d719bc84297e17bace5abd8efe
lakardion <lakardion@protonmail.com>
2023-03-13 11:38:31 -0300

    chore: rm rn mmkv - rm native modules: rn mmkv and fast image which don't work on expo go - add async storage to persis auth state

--- {{cookiecutter.project_slug}}/clients/mobile/react-native/src/services/index.tsx
+++ {{cookiecutter.project_slug}}/clients/mobile/react-native/src/services/index.tsx
@@ -1,36 +1,34 @@
-import React from 'react';
+import React from 'react'
 
-import {OnStartService} from './onStart';
-import {TranslateService} from './translate';
-import {ApiService} from './api';
-import {getNavio} from '../screens';
+import { OnStartService } from './onStart'
+import { TranslateService } from './translate'
+import { getNavio } from '../screens'
 
 class Services {
-  t = new TranslateService();
-  onStart = new OnStartService();
-  api = new ApiService();
+  t = new TranslateService()
+  onStart = new OnStartService()
 
   // -- adding navio as a service
   get navio() {
-    return getNavio();
+    return getNavio()
   }
 }
-export const services = new Services();
+export const services = new Services()
 
-const servicesContext = React.createContext<Services>(services);
-export const ServicesProvider = ({children}: any) => (
+const servicesContext = React.createContext<Services>(services)
+export const ServicesProvider = ({ children }: any) => (
   <servicesContext.Provider value={services}>{children}</servicesContext.Provider>
-);
-export const useServices = (): Services => React.useContext(servicesContext);
+)
+export const useServices = (): Services => React.useContext(servicesContext)
 
 export const initServices = async (): PVoid => {
   for (const key in services) {
     if (Object.prototype.hasOwnProperty.call(services, key)) {
-      const s = (services as any)[key] as IService;
+      const s = (services as any)[key] as IService
 
       if (s.init) {
-        await s.init();
+        await s.init()
       }
     }
   }
-};
+}
