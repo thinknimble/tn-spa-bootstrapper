0e3b4379592b076b0c5c5fff0faf12c0c1b1763b
Ed Romano <oudeis.metis@gmail.com>
2023-03-13 16:57:27 -0400

    tests and import issues

--- {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/core/models.py
+++ {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/core/models.py
@@ -1,5 +1,4 @@
-import uuid  # noqa
-
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.contrib.auth.tokens import default_token_generator
 from django.db import models
