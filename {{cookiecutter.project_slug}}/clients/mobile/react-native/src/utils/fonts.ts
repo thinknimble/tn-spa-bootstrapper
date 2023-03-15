8487219803715345efada7f96065a75019b86327
lakardion <lakardion@protonmail.com>
2023-03-15 09:37:13 -0300

    refactor: attempt to improve font code - but require does not allow variables as an argument, which it is okay but my strings are all known at compile time I'm not doing any shenanigan stuff ffs

--- {{cookiecutter.project_slug}}/clients/mobile/react-native/src/utils/fonts.ts
+++ {{cookiecutter.project_slug}}/clients/mobile/react-native/src/utils/fonts.ts
@@ -1,21 +1,35 @@
+import { FontSource } from 'expo-font'
+
 //TODO: change this to your family, we're using monserrat as base. You could also add more family weights!
 const baseFamily = 'Montserrat' as const
 const fontFormat = 'ttf'
 
-export const customFonts = {
-  [`${baseFamily}-Black` as const]: require(`../../assets/fonts/${baseFamily}-Black.${fontFormat}`),
-  [`${baseFamily}-BlackItalic` as const]: require(`../../assets/fonts/${baseFamily}-BlackItalic.${fontFormat}`),
-  [`${baseFamily}-Bold` as const]: require(`../../assets/fonts/${baseFamily}-Bold.${fontFormat}`),
-  [`${baseFamily}-BoldItalic` as const]: require(`../../assets/fonts/${baseFamily}-BoldItalic.${fontFormat}`),
-  [`${baseFamily}-Italic` as const]: require(`../../assets/fonts/${baseFamily}-Italic.${fontFormat}`),
-  [`${baseFamily}-Light` as const]: require(`../../assets/fonts/${baseFamily}-Light.${fontFormat}`),
-  [`${baseFamily}-LightItalic` as const]: require(`../../assets/fonts/${baseFamily}-LightItalic.${fontFormat}`),
-  [`${baseFamily}-Medium` as const]: require(`../../assets/fonts/${baseFamily}-Medium.${fontFormat}`),
-  [`${baseFamily}-MediumItalic` as const]: require(`../../assets/fonts/${baseFamily}-MediumItalic.${fontFormat}`),
-  [`${baseFamily}-Regular` as const]: require(`../../assets/fonts/${baseFamily}-Regular.${fontFormat}`),
+const fontFamilyVariants = [
+  'Black',
+  'BlackItalic',
+  'Bold',
+  'BoldItalic',
+  'Italic',
+  'Light',
+  'LightItalic',
+  'Medium',
+  'MediumItalic',
+  'Regular',
+] as const
+type FontFamilyVariantValues = typeof fontFamilyVariants[number]
+
+type FontVariantSourceMap = {
+  [K in FontFamilyVariantValues as `${typeof baseFamily}-${K}`]: FontSource
 }
 
-type FontFamily = keyof typeof customFonts
+export const customFonts = Object.fromEntries(
+  fontFamilyVariants.map((ffv) => {
+    const identifier = `${baseFamily}-${ffv}` as const
+    return [identifier, require(`../../assets/${identifier}.${fontFormat}`)]
+  }),
+) as FontVariantSourceMap
+
+type FontFamily = keyof FontVariantSourceMap
 
 export type FontWeightsStyle =
   | 'light'
