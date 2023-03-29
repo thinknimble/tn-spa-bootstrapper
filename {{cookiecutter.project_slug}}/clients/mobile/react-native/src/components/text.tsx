e523764656cbec7959ab6ab5a0b7b3d9d4a013a2
lakardion <lakardion@protonmail.com>
2023-03-29 12:29:40 -0300

    fix(rn) cookiecutter issue with {{

--- {{cookiecutter.project_slug}}/clients/mobile/react-native/src/components/text.tsx
+++ {{cookiecutter.project_slug}}/clients/mobile/react-native/src/components/text.tsx
@@ -5,13 +5,8 @@ import { fontFamilyWeightMap, FontWeightStyle } from '../utils/fonts'
 export const Text: FC<
   Omit<TextProps, 'className'> & { variant?: FontWeightStyle; textClassName?: string }
 > = ({ variant = 'regular', textClassName = '', ...props }) => {
-  return (
-    <TextRN
-      {...props}
-      className={textClassName}
-      style={{
-        fontFamily: fontFamilyWeightMap[variant],
-      }}
-    />
-  )
+  const style = {
+    fontFamily: fontFamilyWeightMap[variant],
+  }
+  return <TextRN {...props} className={textClassName} style={style} />
 }
