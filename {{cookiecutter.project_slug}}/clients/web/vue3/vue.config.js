4f7f07e4eac6878b5c57c49272f28f5e6aae076e
Ed Romano <oudeis.metis@gmail.com>
2023-04-10 16:19:49 -0400

    Vue Hot reloading not working in Docker

--- {{cookiecutter.project_slug}}/clients/web/vue3/vue.config.js
+++ {{cookiecutter.project_slug}}/clients/web/vue3/vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   runtimeCompiler: true,
   assetsDir: 'static',
   devServer: {
+    public: 'localhost:8080',
     proxy: {
       '/api/': {
         target: (process.env.VUE_APP_DEV_SERVER_BACKEND || 'http://server:8000') + '/api',
