bf134d75a502ff9f510e22210ac20016238ea07f
paribaker <58012003+paribaker@users.noreply.github.com>
2023-04-26 14:18:41 +0000

    updated code base for config file output

--- scripts/vue_or_react.sh
+++ scripts/vue_or_react.sh
@@ -12,8 +12,9 @@ if [ $react_count -gt $vue_count ]; then
 else
   config_file_path="cookiecutter/vue_template.yaml"
 fi
-ls -a cookiecutter/
+
 if [ "$rn_count" != 0 ]; then 
   sed -i.bak 's/include_mobile: "n"/include_mobile: "y"/' $config_file_path
 fi
+
 echo $config_file_path
