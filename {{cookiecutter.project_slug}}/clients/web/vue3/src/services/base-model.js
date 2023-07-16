1d8c8eb4237e3f0dd6bec5364e65b2da5f54c971
paribaker <58012003+paribaker@users.noreply.github.com>
2023-07-16 20:43:00 +0000

    make base shape readonly

--- {{cookiecutter.project_slug}}/clients/web/vue3/src/services/base-model.js
+++ {{cookiecutter.project_slug}}/clients/web/vue3/src/services/base-model.js
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import {readonly} from '@thinknimble/tn-models-fp'
 
 export const baseModelShape = {
-  id: z.string().uuid(),
+  id: readonly(z.string().uuid()),
   datetimeCreated: readonly(z.string().datetime().optional()),
   lastEdited: readonly(z.string().datetime().optional()),
 }
