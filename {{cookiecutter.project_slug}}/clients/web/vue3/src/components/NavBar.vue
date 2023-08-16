5b56081938532595b16fb89fc85d84e868396f16
mayakeeley <maya@thinknimble.com>
2023-08-16 09:11:23 -0500

    revert navbar

--- {{cookiecutter.project_slug}}/clients/web/vue3/src/components/NavBar.vue
+++ {{cookiecutter.project_slug}}/clients/web/vue3/src/components/NavBar.vue
@@ -21,8 +21,8 @@
         </div>
         <div class="hidden sm:ml-6 sm:flex sm:items-center">
           <template v-if="!isLoggedIn">
-            <router-link :to="{ name: 'Login' }" class="btn--primary bg-primary">Login</router-link>
-            <router-link :to="{ name: 'Signup' }" class="btn--secondary ml-6">Signup</router-link>
+            <router-link :to="{ name: 'Login' }" class="btn--primary" data-cy="login">Login</router-link>
+            <router-link :to="{ name: 'Signup' }" class="btn--secondary">Signup</router-link>
           </template>
           <!-- Profile dropdown -->
           <div class="relative ml-3 focus:ring-2" v-if="isLoggedIn">
@@ -103,20 +103,10 @@
         </div>
         <div class="mt-3 space-y-1">
           <template v-if="!isLoggedIn">
-            <router-link
-              @click="mobileMenuOpen = false"
-              :to="{ name: 'Signup' }"
-              active-class="active--mobile"
-              class="mobile-link"
-            >
+            <router-link @click="mobileMenuOpen = false" :to="{ name: 'Signup' }" active-class="active--mobile" class="mobile-link">
               Signup
             </router-link>
-            <router-link
-              @click="mobileMenuOpen = false"
-              :to="{ name: 'Login' }"
-              active-class="active--mobile"
-              class="mobile-link"
-            >
+            <router-link @click="mobileMenuOpen = false" :to="{ name: 'Login' }" data-cy="login" active-class="active--mobile" class="mobile-link">
               Login
             </router-link>
           </template>
@@ -142,6 +132,7 @@ export default {
 
     function logout() {
       profileMenuOpen.value = false
+      mobileMenuOpen.value = false
       store.dispatch('setUser', null)
       router.push({ name: 'Home' })
     }
