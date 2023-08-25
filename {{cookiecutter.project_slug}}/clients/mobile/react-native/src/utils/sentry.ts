1256e2cf13e2b7dd73788daab85f9d64991a9813
lakardion <lakardion@protonmail.com>
2023-08-25 14:17:57 -0300

    chore(mobile): add sentryDSN from config

--- {{cookiecutter.project_slug}}/clients/mobile/react-native/src/utils/sentry.ts
+++ {{cookiecutter.project_slug}}/clients/mobile/react-native/src/utils/sentry.ts
@@ -1,8 +1,9 @@
 import * as Sentry from 'sentry-expo'
+import Config from '../../Config'
 
 export const initSentry = () => {
   Sentry.init({
-    dsn: '', //TODO: add your dsn
+    dsn: Config.sentryDSN,
     enableInExpoDevelopment: true,
     debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
   })
