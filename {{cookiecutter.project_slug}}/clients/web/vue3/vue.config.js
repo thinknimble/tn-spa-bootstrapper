4e04bf637824bf49f6c1f8509e9d6326588bde89
David Liu <r2dliu@Davids-MacBook-Pro.local>
2023-08-28 12:03:41 -0400

    remove breaking change field

--- {{cookiecutter.project_slug}}/clients/web/vue3/vue.config.js
+++ {{cookiecutter.project_slug}}/clients/web/vue3/vue.config.js
@@ -2,7 +2,6 @@ module.exports = {
   runtimeCompiler: true,
   assetsDir: 'static',
   devServer: {
-    public: 'localhost:8080',
     proxy: {
       '/api/': {
         target: (process.env.VUE_APP_DEV_SERVER_BACKEND || 'http://server:8000') + '/api',
