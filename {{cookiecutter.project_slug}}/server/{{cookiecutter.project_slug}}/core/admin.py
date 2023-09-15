69950fc4722fa5609fc5e1055061d1a69fc1511f
Franco Pelusso <francokuchiki@gmail.com>
2023-09-15 14:55:01 -0300

    feat: add examples on how to use the admin autocomplete filter

--- {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/core/admin.py
+++ {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/core/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.models import Group, Permission
 
+from {{cookiecutter.project_slug}}.common.admin.filters import AutocompleteFilter, AutocompleteAdminMedia
 from .models import User
 
 
@@ -44,6 +46,7 @@ class CustomUserAdmin(UserAdmin):
         "last_name",
     )
     list_filter = (
+        ("groups", AutocompleteFilter),
         "is_active",
         "is_staff",
         "is_superuser",
@@ -51,5 +54,27 @@ class CustomUserAdmin(UserAdmin):
 
     ordering = []
 
+    class Media(AutocompleteAdminMedia):
+        pass
+
+
+class CustomGroupAdmin(GroupAdmin):
+    list_filter = (("permissions", AutocompleteFilter), )
+
+    class Media(AutocompleteAdminMedia):
+        pass
+
+
+# If we want the autocomplete filter by permission on the Group admin to work, we need to include an admin
+# site for the permissions themselves, and we definitely need to define the search_fields.
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    search_fields = ("name", "content_type__app_label", "content_type__model", "codename")
+
 
 admin.site.register(User, CustomUserAdmin)
+
+# Since Django's contrib apps already register Group to the admin site, we need to remove it and add it again
+# but with our custom admin class.
+admin.site.unregister(Group)
+admin.site.register(Group, CustomGroupAdmin)
