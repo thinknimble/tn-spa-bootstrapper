dca5e6b69885283fe799859444edec978b1c5816
Edwin Noriega <adolfonoriega9@gmail.com>
2023-09-15 15:49:00 -0600

    change relative imports to absolute

--- {{cookiecutter.project_slug}}/clients/mobile/react-native/src/utils/providers.tsx
+++ {{cookiecutter.project_slug}}/clients/mobile/react-native/src/utils/providers.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren } from 'react'
 
-import { ServicesProvider } from '../services'
+import { ServicesProvider } from '@services/index'
 
 export const SSProvider: React.FC<PropsWithChildren<{}>> = ({ children }) => {
   return <ServicesProvider>{children}</ServicesProvider>
