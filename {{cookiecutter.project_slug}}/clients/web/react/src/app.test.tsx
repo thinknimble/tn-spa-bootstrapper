c271ff8a7b92ba03c948ba636cb234329cadb88f
Damian <52294448+lakardion@users.noreply.github.com>
2023-10-13 15:18:04 -0300

    Remove GQL alternative from react client (#230)

--- {{cookiecutter.project_slug}}/clients/web/react/src/app.test.tsx
+++ {{cookiecutter.project_slug}}/clients/web/react/src/app.test.tsx
@@ -2,20 +2,11 @@ import { screen } from '@testing-library/react'
 import { App } from './app'
 import { render } from './test-utils'
 import './utils/__tests__/mediaMock'
-{% if cookiecutter.use_graphql == 'y' -%}
-import { MockedProvider } from '@apollo/client/testing'
-{% endif -%}
 
 describe('App test', () => {
   test('Renders login', () => {
     render(
-{% if cookiecutter.use_graphql == 'y' -%}
-      <MockedProvider>
-        <App />
-      </MockedProvider>
-{% else -%}
       <App/>
-{% endif -%}
     )
     expect(screen.getByText('PORTAL LOG IN')).toBeTruthy()
   })
