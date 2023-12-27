0569fb1ea829b77b9474e27e3839f2409367eee4
Edwin Noriega <adolfonoriega9@gmail.com>
2023-12-27 00:13:13 -0600

    Update home page text

--- {{cookiecutter.project_slug}}/clients/web/react/src/pages/home.tsx
+++ {{cookiecutter.project_slug}}/clients/web/react/src/pages/home.tsx
@@ -5,11 +5,11 @@ export const Home = () => {
     <>
       <div className="mx-auto flex min-h-full max-w-2xl flex-col items-center px-6 py-20 sm:py-48 lg:px-8">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-          Welcome to {{ cookiecutter.project_name }}!
+          Welcome to {`{{ cookiecutter.project_name }}`}!
         </h1>
         <p className="mt-6 text-lg leading-8 text-gray-600">
-          Here&apos;s some information about {{ cookiecutter.project_name }}. Please update and expand on this text. This text is
-          the first thing that users will see on the home page.
+          Here&apos;s some information about {`{{ cookiecutter.project_name }}`}. Please update and
+          expand on this text. This text is the first thing that users will see on the home page.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <Link to="/dashboard" className="btn--accent">
