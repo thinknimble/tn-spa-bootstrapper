4a4fe5361b7c9af9ec1549738a01b0142e0c695a
Edwin Noriega <adolfonoriega9@gmail.com>
2023-12-27 13:38:17 -0600

    Remove app.test.tsx and update test_login.js

--- {{cookiecutter.project_slug}}/clients/web/react/tests/e2e/specs/test_login.js
+++ {{cookiecutter.project_slug}}/clients/web/react/tests/e2e/specs/test_login.js
@@ -1,12 +1,13 @@
 describe('Tests login workflow', () => {
-  it('Home page auto redirects to login', () => {
+  it('Home page has link to login', () => {
     cy.visit('/')
+    cy.get('[data-cy="login-btn"]').click()
     cy.url().should('include', '/log-in')
   })
   it('Filling in email and passwords goes to', () => {
     cy.get('[data-cy=email]').type(Cypress.env('TEST_USER_EMAIL'))
     cy.get('[data-cy=password]').type(Cypress.env('TEST_USER_PASS'))
-    cy.contains('[data-cy="login-btn"]', 'Login').click()
+    cy.contains('[data-cy="login-btn"]', 'Log in').click()
     cy.url().should('include', '/home')
   })
 })
