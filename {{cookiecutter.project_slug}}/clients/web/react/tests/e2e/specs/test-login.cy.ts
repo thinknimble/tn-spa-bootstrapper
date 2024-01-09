c4fd3d07f0c3c5b1f1ccc00949284771c059d197
thin-paribaker <pari@thinknimble.com>
2024-01-08 21:43:37 -0800

    updated react to follow new cypress as is used in vue

--- {{cookiecutter.project_slug}}/clients/web/react/tests/e2e/specs/test-login.cy.ts
+++ {{cookiecutter.project_slug}}/clients/web/react/tests/e2e/specs/test-login.cy.ts
@@ -2,12 +2,9 @@ describe('Tests login workflow', () => {
   it('Home page auto redirects to login', () => {
     cy.visit('/')
     cy.url().should('include', '/log-in')
-  })
-  it('Filling in email and passwords goes to', () => {
     cy.get('[data-cy=email]').type(Cypress.env('TEST_USER_EMAIL'))
     cy.get('[data-cy=password]').type(Cypress.env('TEST_USER_PASS'))
-    cy.get('[data-cy="login-btn"]').click()
-    cy.url().should('include', '/home')
+    cy.contains('[data-cy="login-btn"]', 'Login').click()
+    cy.url().should('include', '/test')
   })
 })
- 
