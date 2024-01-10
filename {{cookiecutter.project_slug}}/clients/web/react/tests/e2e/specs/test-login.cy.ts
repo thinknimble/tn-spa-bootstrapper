cb85c125cfd7db90d7938f7eb07cd034f0c79f66
thin-paribaker <pari@thinknimble.com>
2024-01-09 16:13:29 -0800

    continue with tests

--- {{cookiecutter.project_slug}}/clients/web/react/tests/e2e/specs/test-login.cy.ts
+++ {{cookiecutter.project_slug}}/clients/web/react/tests/e2e/specs/test-login.cy.ts
@@ -2,6 +2,9 @@ describe('Tests login workflow', () => {
   it('Home page auto redirects to login', () => {
     cy.visit('/')
     cy.url().should('include', '/log-in')
+  }),
+  it('Home page auto redirects to login', () => {
+    cy.visit('/log-in')
     cy.get('[data-cy=email]').type(Cypress.env('TEST_USER_EMAIL'))
     cy.get('[data-cy=password]').type(Cypress.env('TEST_USER_PASS'))
     cy.contains('[data-cy="login-btn"]').click()
