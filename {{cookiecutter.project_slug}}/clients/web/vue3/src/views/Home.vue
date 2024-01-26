b34d869d4434f2c6ce8f24b784c99ab46efc3329
lakardion <lakardion@protonmail.com>
2024-01-26 14:01:10 -0300

    fix prettier yet again...

--- {{cookiecutter.project_slug}}/clients/web/vue3/src/views/Home.vue
+++ {{cookiecutter.project_slug}}/clients/web/vue3/src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="mx-auto flex min-h-full max-w-2xl flex-col items-center px-6 py-20 sm:py-48 lg:px-8">
-    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Welcome to {{ cookiecutter.project_name }}!</h1>
+    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+      Welcome to {{ cookiecutter.project_name }}!
+    </h1>
     <p class="mt-6 text-lg leading-8 text-gray-600">
-      Here's some information about {{ cookiecutter.project_name }}. Please update and expand on this text. This text is the first thing that users will see on the home page.
+      Here's some information about {{ cookiecutter.project_name }}. Please update and expand on this text. This text is 
+      the first thing that users will see on the home page.
     </p>
     <div class="mt-10 flex items-center justify-center gap-x-6">
       <router-link :to="{ name: 'Dashboard' }" class="btn--accent">Get started</router-link>
