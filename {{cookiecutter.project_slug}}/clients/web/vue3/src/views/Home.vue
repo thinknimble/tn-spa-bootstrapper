f8401a825d0420631fada4d2987b7d27e9f05b28
lakardion <lakardion@protonmail.com>
2024-01-26 14:09:31 -0300

    fix prettier

--- {{cookiecutter.project_slug}}/clients/web/vue3/src/views/Home.vue
+++ {{cookiecutter.project_slug}}/clients/web/vue3/src/views/Home.vue
@@ -4,7 +4,7 @@
       Welcome to {{ cookiecutter.project_name }}!
     </h1>
     <p class="mt-6 text-lg leading-8 text-gray-600">
-      Here's some information about {{ cookiecutter.project_name }}. Please update and expand on this text. This text is 
+      Here's some information about {{ cookiecutter.project_name }}. Please update and expand on this text. This text is
       the first thing that users will see on the home page.
     </p>
     <div class="mt-10 flex items-center justify-center gap-x-6">
