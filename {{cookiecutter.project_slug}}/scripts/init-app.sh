33213d29984ade091fe32de749e5d4639cc2501f
Damian <lakardion@protonmail.com>
2024-04-05 18:20:51 +0200

    bun instead of npm

--- {{cookiecutter.project_slug}}/scripts/init-app.sh
+++ {{cookiecutter.project_slug}}/scripts/init-app.sh
@@ -6,7 +6,7 @@
 # Applying the migrations
 {%- if cookiecutter.client_app.lower() != "none" %}
 # Generating client app
-npm install --prefix client && npm run build --prefix client
+bun install --prefix client && bun run build --prefix client
 {%- endif %}
 pipenv install
 pipenv run python server/manage.py makemigrations && pipenv run python server/manage.py migrate 
