d7d4e48a0017cba8384ce1c616f8b1dccafe5a6b
Damian <lakardion@protonmail.com>
2024-05-22 14:54:56 +0200

    chore(vue) upgrade tn-models to v3

--- {{cookiecutter.project_slug}}/clients/web/vue3/src/services/users/api.js
+++ {{cookiecutter.project_slug}}/clients/web/vue3/src/services/users/api.js
@@ -1,51 +1,43 @@
 import { createApi, createCustomServiceCall } from '@thinknimble/tn-models'
 import { z } from 'zod'
 import axiosInstance from '../AxiosClient'
-import { userShape, forgotPasswordShape, userCreateShape, loginShape } from './models'
+import { forgotPasswordShape, loginShape, userCreateShape, userShape } from './models'
 
-const login = createCustomServiceCall(
-  {
-    inputShape: loginShape,
-    outputShape: userShape,
-  },
-  async ({ client, input, utils }) => {
+const login = createCustomServiceCall({
+  inputShape: loginShape,
+  outputShape: userShape,
+  cb: async ({ client, input, utils }) => {
     const res = await client.post('/login/', utils.toApi(input))
     return utils.fromApi(res.data)
   },
-)
+})
 
-const requestPasswordReset = createCustomServiceCall(
-  {
-    inputShape: forgotPasswordShape,
-  },
-  async ({ client, input }) => {
+const requestPasswordReset = createCustomServiceCall({
+  inputShape: forgotPasswordShape,
+  cb: async ({ client, input }) => {
     await client.get(`/password/reset/${input.email}/`)
   },
-)
-const resetPassword = createCustomServiceCall(
-  {
-    inputShape: { uid: z.string().email(), token: z.string(), password: z.string() },
-    outputShape: userShape,
-  },
-  async ({ client, input, utils }) => {
-    const { email, ...rest } = utils.toApi(input)
-    const res = await client.post(`/password/reset/code/confirm/${email}/`, rest)
+})
+const resetPassword = createCustomServiceCall({
+  inputShape: { uid: z.string().email(), token: z.string(), password: z.string() },
+  outputShape: userShape,
+  cb: async ({ client, input, utils }) => {
+    const { uid, ...rest } = utils.toApi(input)
+    const res = await client.post(`/password/reset/code/confirm/${uid}/`, rest)
     return utils.fromApi(res.data)
   },
-)
+})
 
 const logout = createCustomServiceCall(async ({ client }) => {
   return client.post(`/logout/`)
 })
 
-export const userApi = createApi(
-  {
-    client: axiosInstance,
-    baseUri: '/users/',
-    models: {
-      create: userCreateShape,
-      entity: userShape,
-    },
+export const userApi = createApi({
+  client: axiosInstance,
+  baseUri: '/users/',
+  models: {
+    create: userCreateShape,
+    entity: userShape,
   },
-  { login, requestPasswordReset, resetPassword, logout },
-)
+  customCalls: { login, requestPasswordReset, resetPassword, logout },
+})
