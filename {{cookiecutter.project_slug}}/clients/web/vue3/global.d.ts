9650c1d40badef2c1125ad55f446d469cedcd3e7
paribaker <58012003+paribaker@users.noreply.github.com>
2024-05-30 10:34:38 -0700

    upate the input fields, add vue alert (#297)

---
+++ {{cookiecutter.project_slug}}/clients/web/vue3/global.d.ts
@@ -0,0 +1,1 @@
+declare module '@thinknimble/vue3-alert-alert';
--- {{cookiecutter.project_slug}}/clients/web/vue3/src/main.ts
+++ {{cookiecutter.project_slug}}/clients/web/vue3/src/main.ts
@@ -3,5 +3,7 @@ import './main.css'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import '@thinknimble/vue3-alert-alert/dist/vue3-alert-alert.css'
+import AlertPlugin from '@thinknimble/vue3-alert-alert'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(AlertPlugin, {}).use(store).use(router).mount('#app')
