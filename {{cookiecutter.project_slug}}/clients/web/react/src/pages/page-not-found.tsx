734a4ef2cc7251cd4c24ac083c1000ecc59f031e
Edwin Noriega <adolfonoriega9@gmail.com>
2024-06-04 12:37:18 -0600

    chore: Update link to home page in 404 error page

--- {{cookiecutter.project_slug}}/clients/web/react/src/pages/page-not-found.tsx
+++ {{cookiecutter.project_slug}}/clients/web/react/src/pages/page-not-found.tsx
@@ -4,15 +4,15 @@ export const PageNotFound = () => {
   return (
     <main className="grid h-full flex-auto place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <p className="text-accent font-semibold">404</p>
+        <p className="font-semibold text-accent">404</p>
         <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">Page not found</h1>
         <p className="mt-6 leading-7">
           Sorry, we couldn&apos;t find the page you&apos;re looking for.
         </p>
         <div className="mt-10 gap-x-6">
           <Link
-            to="/"
-            className="bg-accent hover:bg-primary focus-visible:outline-primary rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+            to="/home"
+            className="rounded-md bg-accent px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
           >
             Go back home
           </Link>
