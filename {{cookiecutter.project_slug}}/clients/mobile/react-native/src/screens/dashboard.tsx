8b658ebbbd5c00e37efe55e484f1d1413ae574b0
Damian <lakardion@protonmail.com>
2024-06-25 18:59:36 +0200

    feat(rn) add useLogout hook, remove logout

--- {{cookiecutter.project_slug}}/clients/mobile/react-native/src/screens/dashboard.tsx
+++ {{cookiecutter.project_slug}}/clients/mobile/react-native/src/screens/dashboard.tsx
@@ -1,10 +1,29 @@
 import { MultiPlatformSafeAreaView } from '@components/multi-platform-safe-area-view'
 import { Text } from '@components/text'
+import { Pressable } from 'react-native'
 import React from 'react'
+import { navioAtom } from '@stores/navigation'
+import { useAtomValue } from 'jotai'
+import { useLogout } from '@services/user'
+
 export const DashboardScreen = () => {
+  const navio = useAtomValue(navioAtom)
+  const { mutate: logout, isPending: isLoggingOut } = useLogout()
+
   return (
     <MultiPlatformSafeAreaView>
-      <Text>Welcom to the Dash</Text>
+      <Text>Welcome to the Dashboard</Text>
+      <Pressable onPress={()=>{
+         logout(undefined,{
+          onSettled:()=>{
+            navio?.setRoot('stacks', 'AuthStack')
+          }
+         })
+      }}>
+        <Text textClassName="text-primary">
+          Log out
+        </Text>
+      </Pressable>
     </MultiPlatformSafeAreaView>
   )
 }
