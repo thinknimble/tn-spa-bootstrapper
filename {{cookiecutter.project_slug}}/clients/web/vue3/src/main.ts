62cd289d08ef29b31bd8c80700dd082611778f1c
David Liu <david.liu@thinknimble.com>
2024-07-19 08:50:19 -0400

    update to vue 3.4. removed deprecated libraries and migrate (#316)
    Co-authored-by: David Liu <r2dliu@MacBook-Pro-4.local>
    Co-authored-by: paribaker <58012003+paribaker@users.noreply.github.com>

--- {{cookiecutter.project_slug}}/clients/web/vue3/src/main.ts
+++ {{cookiecutter.project_slug}}/clients/web/vue3/src/main.ts
@@ -1,15 +1,16 @@
 import { createApp } from 'vue'
-import './main.css'
-import App from './App.vue'
-import store from './store'
-import router from './router'
-import '@thinknimble/vue3-alert-alert/dist/vue3-alert-alert.css'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import AlertPlugin from '@thinknimble/vue3-alert-alert'
 import { VueQueryPlugin } from '@tanstack/vue-query'
 
-createApp(App)
-    .use(AlertPlugin, {})
-    .use(VueQueryPlugin)
-    .use(store)
-    .use(router)
-    .mount('#app')
+import './main.css'
+import App from './App.vue'
+import router from './router'
+
+import '@thinknimble/vue3-alert-alert/dist/vue3-alert-alert.css'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+createApp(App).use(pinia).use(AlertPlugin, {}).use(VueQueryPlugin).use(router).mount('#app')
