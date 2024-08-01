aee1ffc0dbcafc5fe1e4bcd3b7b66875926b01b2
Edward Romano <oudeismetis@users.noreply.github.com>
2024-08-01 18:15:19 -0400

    Improve several small quality of life items for devs (#331)

--- {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/core/forms.py
+++ {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/core/forms.py
@@ -1,5 +1,31 @@
 from django import forms
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+
+User = get_user_model()
 
 
 class PreviewTemplateForm(forms.Form):
     _send_to = forms.EmailField(label="Send by email to", widget=forms.EmailInput(attrs={"size": 60}))
+
+
+class GroupAdminForm(forms.ModelForm):
+    users = forms.ModelMultipleChoiceField(queryset=User.objects.all(), required=False, widget=FilteredSelectMultiple("users", False))
+
+    def __init__(self, *args, **kwargs):
+        super(GroupAdminForm, self).__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields["users"].initial = self.instance.user_set.all()
+
+    def save_m2m(self):
+        self.instance.user_set.set(self.cleaned_data["users"])
+
+    def save(self, *args, **kwargs):
+        instance = super(GroupAdminForm, self).save()
+        self.save_m2m()
+        return instance
+
+    class Meta:
+        model = Group
+        exclude = []
