485952c4747d3bd6dd94ee77d4e1e373c10c8408
Pari Work Temp <pariwork@thinknimble.com>
2024-08-13 13:13:29 -0500

    reset var on exit and default to es

--- {{cookiecutter.project_slug}}/scripts/setup_mobile_config.sh
+++ {{cookiecutter.project_slug}}/scripts/setup_mobile_config.sh
@@ -15,6 +15,11 @@ defaults_file=$2
 declare -a config_vars
 declare -a merged_arr
 
+reset_config_vars() {
+    unset config_vars
+}
+trap reset_config_vars EXIT
+
 while IFS= read -r line; do
     config_vars+=("$line")
 done < <(grep -o "REPLACE_WITH_[A-Z_]*" "$config_file")
@@ -25,7 +30,7 @@ done < <(grep -o "REPLACE_WITH_[A-Z_]*" "$config_file")
 
 
 for i in "${config_vars[@]}"; do
-    value=$(grep -o "$i=.*" "$defaults_file")
+    value=$(grep -o "$i=.*" "$defaults_file") || echo ""
     if [[ -z $value ]]; then
         echo "Skipping $i as it does not exist in $defaults_file"
         continue
