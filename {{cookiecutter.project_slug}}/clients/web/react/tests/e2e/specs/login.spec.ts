9ab61eeaad514f69615bebff2906193f86d579e8
Edwin Noriega <adolfonoriega9@gmail.com>
2024-08-15 11:17:48 -0600

    chore: Update login.spec.ts to use expect assertion for PLAYWRIGHT_TEST_USER_PASS

--- {{cookiecutter.project_slug}}/clients/web/react/tests/e2e/specs/login.spec.ts
+++ {{cookiecutter.project_slug}}/clients/web/react/tests/e2e/specs/login.spec.ts
@@ -3,9 +3,7 @@ import { test, expect } from '@playwright/test'
 import dotenv from 'dotenv'
 
 test('Login workflow', async ({ page }) => {
-  if (!process.env.PLAYWRIGHT_TEST_USER_PASS) {
-    throw new Error('PLAYWRIGHT_TEST_USER_PASS env var is not set')
-  }
+  expect(process.env.PLAYWRIGHT_TEST_USER_PASS).toBeTruthy()
 
   await page.goto('/log-in')
   await page.getByTestId('email').fill('playwright@thinknimble.com')
