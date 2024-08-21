36cb1aa9e80d1db011ff17dc5584949d01b23a38
Edwin Noriega <adolfonoriega9@gmail.com>
2024-08-21 17:00:55 -0600

    chore: Update environment variable name for test user password

--- {{cookiecutter.project_slug}}/clients/web/vue3/tests/e2e/specs/login.spec.ts
+++ {{cookiecutter.project_slug}}/clients/web/vue3/tests/e2e/specs/login.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test'
 
 test('Login workflow', async ({ page }) => {
-  expect(process.env.PLAYWRIGHT_TEST_USER_PASS).toBeTruthy()
+  expect(process.env.CYPRESS_TEST_USER_PASS).toBeTruthy()
 
   await page.goto('/login')
   await page.getByPlaceholder('Enter email...').fill('playwright@thinknimble.com')
-  await page.getByPlaceholder('Enter password...').fill(process.env.PLAYWRIGHT_TEST_USER_PASS ?? '')
+  await page.getByPlaceholder('Enter password...').fill(process.env.CYPRESS_TEST_USER_PASS ?? '')
   await page.getByRole('button', { name: 'Log in' }).click()
 
   await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible()
