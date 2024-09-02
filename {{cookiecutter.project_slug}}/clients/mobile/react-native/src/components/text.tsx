1ba0d4e42384f4a7e0109f2ccc5f96d90f9f2d2e
Elias Biagioni <elias.biagioni@newcombin.com>
2024-09-02 17:35:14 -0300

    Add missings

---
+++ {{cookiecutter.project_slug}}/clients/mobile/react-native/src/components/text.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import { Text as TextRN, TextProps } from 'react-native'
+import { fontFamilyWeightMap, FontWeightStyle } from '@utils/fonts'
+
+export const Text: FC<
+  Omit<TextProps, 'className'> & { variant?: FontWeightStyle; textClassName?: string }
+> = ({ variant = 'regular', textClassName = '', ...props }) => {
+  const style = {
+    fontFamily: fontFamilyWeightMap[variant],
+  }
+  return <TextRN {...props} className={textClassName} style={style} />
+}
