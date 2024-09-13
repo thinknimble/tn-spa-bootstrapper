f3a542f87f5380091a207843da01b4555c370cec
oudeismetis <edward@thinknimble.com>
2024-09-13 11:54:04 -0400

    Decouple session management of tokens from user data

--- {{cookiecutter.project_slug}}/clients/web/vue3/src/stores/user.ts
+++ {{cookiecutter.project_slug}}/clients/web/vue3/src/stores/user.ts
@@ -1,5 +1,6 @@
 // Pinia Store
 import { defineStore } from 'pinia'
+import { z } from 'zod'
 import { UserShape } from '@/services/users'
 
 const STORAGE_HASH = '{{ random_ascii_string(10) }}'
@@ -7,11 +8,13 @@ export const STORAGE_KEY = `{{ cookiecutter.project_slug }}-${STORAGE_HASH}`
 
 interface State {
   user: UserShape | null
+  token: z.string() | null
 }
 
 export const useUserStore = defineStore('user', {
   state: (): State => ({
     user: null,
+    token: null,
   }),
   persist: {
     key: STORAGE_KEY,
@@ -21,12 +24,16 @@ export const useUserStore = defineStore('user', {
       return !!state.user
     },
     token: (state) => {
-      return state.user ? state.user.token : null
+      return state.token
     },
   },
   actions: {
     updateUser(payload: UserShape) {
-      this.user = payload
+      const { token, ...userData } = payload
+      this.user = userData
+      if (token) {
+        this.token = token
+      }
     },
     clearUser() {
       this.$reset()
