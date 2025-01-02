ba79c7b2008ee5f9f081db4c1c7efc1e88da00b3
Pari Work Temp <pariwork@thinknimble.com>
2025-01-02 10:53:37 -0500

    added window/mobile utility composables

---
+++ {{cookiecutter.project_slug}}/clients/web/vue3/src/composables/Window.ts
@@ -0,0 +1,61 @@
+import { onMounted, onUnmounted, Ref, ref, watchEffect } from 'vue'
+
+export function useWindowSize() {
+  const width = ref(window.innerWidth)
+  const height = ref(window.innerHeight)
+  const update = () => {
+    width.value = window.innerWidth
+    height.value = window.innerHeight
+  }
+  onMounted(() => {
+    window.addEventListener('resize', update)
+  })
+  onUnmounted(() => {
+    window.removeEventListener('resize', update)
+  })
+  return { width, height }
+}
+
+export const useIsMobile = () => {
+  const { width } = useWindowSize()
+  const isMobile = ref(width.value < 768)
+  watchEffect(() => {
+    isMobile.value = width.value < 768
+  })
+
+  return { isMobile, width }
+}
+
+export const useElementSize = (element: Ref<HTMLElement | null>) => {
+  const width = ref(0)
+  const height = ref(0)
+  let observer: ResizeObserver | null = null
+
+  const updateSize = (entries: ResizeObserverEntry[]) => {
+    const entry = entries[0]
+    width.value = entry.contentRect.width
+    height.value = entry.contentRect.height
+  }
+
+  watchEffect(() => {
+    if (element.value) {
+      const entry = { contentRect: element.value.getBoundingClientRect() }
+      updateSize([entry as ResizeObserverEntry])
+    }
+  })
+
+  onMounted(async () => {
+    if (element.value) {
+      observer = new ResizeObserver(updateSize)
+      observer.observe(element.value)
+    }
+  })
+
+  onUnmounted(() => {
+    if (observer && element.value) {
+      observer.disconnect()
+    }
+  })
+
+  return { width, height }
+}
