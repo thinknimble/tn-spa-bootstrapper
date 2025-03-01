0a346e5e89d1e23a366f01cbe81f9c5673b06b50
William Huster <whusterj@gmail.com>
2025-03-01 14:51:21 -0500

    WIP

--- scripts/buildpack_run.sh
+++ scripts/buildpack_run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl -LsSf https://astral.sh/uv/install.sh | sh
+# curl -LsSf https://astral.sh/uv/install.sh | sh
+export PATH=$HOME/.local/bin:$PATH
 uvx cookiecutter . --config-file cookiecutter/react_template.yaml --no-input -f
