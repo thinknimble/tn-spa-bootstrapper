6d6dfbe21c896ecb2c95169f4e9f9fdc00e4c9a6
William Huster <whusterj@gmail.com>
2025-03-03 16:14:25 -0500

    typo

--- {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/chat/views.py
+++ {{cookiecutter.project_slug}}/server/{{cookiecutter.project_slug}}/chat/views.py
@@ -2,14 +2,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .exceptions import BadTemplateException
-from .models import CoachPromptTemplate
+from .models import PromptTemplate
 from .serializers import SystemPromptSerializer
 
 
 @api_view(["GET"])
 def get_current_system_prompt(request):
     try:
-        assembled_prompt = CoachPromptTemplate.objects.get_assembled_prompt()
+        assembled_prompt = PromptTemplate.objects.get_assembled_prompt()
     except BadTemplateException as e:
         return Response({"error": str(e)}, status=500)
 
