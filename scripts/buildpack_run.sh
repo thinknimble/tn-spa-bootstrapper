282e1088c40889792274da83e0c19a6042123401
William Huster <whusterj@gmail.com>
2025-03-03 13:10:18 -0500

    Start migration WSGI to ASGI

--- scripts/buildpack_run.sh
+++ scripts/buildpack_run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# curl -LsSf https://astral.sh/uv/install.sh | sh
-export PATH=$HOME/.local/bin:$PATH
+export PATH=$HOME/.local/bin:$PATH  # uv and uvx are installed in this directory
 uvx cookiecutter . --config-file cookiecutter/react_template.yaml --no-input -f
