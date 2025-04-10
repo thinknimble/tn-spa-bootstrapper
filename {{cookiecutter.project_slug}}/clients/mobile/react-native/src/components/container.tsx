22b0da5d97a02c924f1e6378956d820e69dce682
Damian <lakardion@protonmail.com>
2025-04-10 13:59:23 +0200

    fix: error on component

--- {{cookiecutter.project_slug}}/clients/mobile/react-native/src/components/container.tsx
+++ {{cookiecutter.project_slug}}/clients/mobile/react-native/src/components/container.tsx
@@ -1,33 +1,27 @@
-import { Form, FormFieldsRecord, IFormField } from '@thinknimble/tn-forms'
-import { useTnForm } from '@thinknimble/tn-forms-react'
-import { Text, TextInput, TextInputProps, View } from 'react-native'
-import twColors from 'tailwindcss/colors'
-import { ErrorsList } from '@components/errors'
+import { FC, ReactNode } from 'react'
+import { View } from 'react-native'
+import { MultiPlatformSafeAreaView } from './multi-platform-safe-area-view'
 import { cn } from '@utils/style'
 
-export const TextFormField = <T extends IFormField<string>, TForm extends Form<FormFieldsRecord>>({
-  field,
-  containerClassName,
-  ...textInputProps
-}: { field: T; containerClassName?: string } & TextInputProps) => {
-  const { createFormFieldChangeHandler } = useTnForm<TForm>()
+export const Container: FC<{
+  children: ReactNode
+  containerClassName?: string
+  innerContainerClassName?: string
+  hasHorizontalPadding?: boolean
+}> = ({ children, containerClassName, innerContainerClassName, hasHorizontalPadding = true }) => {
   return (
-    <View className={containerClassName}>
-      <Text className="text-lg font-primary-medium">{field.label}</Text>
-      <View className="w-full">
-        <TextInput
-          {...textInputProps}
-          value={field.value}
-          onChangeText={createFormFieldChangeHandler(field)}
-          placeholderTextColor={twColors.slate['400']}
-          placeholder={field.placeholder}
-          className={cn([
-            'bg-white w-full text-lg border text-gray-700 rounded p-2',
-            textInputProps.className,
-          ])}
-        />
+    <MultiPlatformSafeAreaView
+      safeAreaClassName={cn(['h-full flex-1 flex-grow', containerClassName])}
+    >
+      <View
+        className={cn([
+          'flex-1 flex-grow',
+          hasHorizontalPadding ? 'px-4' : '',
+          innerContainerClassName,
+        ])}
+      >
+        {children}
       </View>
-      <ErrorsList errors={field.errors} containerClassName="pt-1" />
-    </View>
+    </MultiPlatformSafeAreaView>
   )
 }
