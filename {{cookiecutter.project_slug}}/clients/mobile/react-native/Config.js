d083e8a8774bd9520155cb89086cc858256d2767
Pari Work Temp <pariwork@thinknimble.com>
2025-06-30 13:36:42 -0700

    updates to certain files

--- {{cookiecutter.project_slug}}/clients/mobile/react-native/Config.js
+++ {{cookiecutter.project_slug}}/clients/mobile/react-native/Config.js
@@ -5,9 +5,8 @@ import Logger from './logger'
 
 const BACKEND_SERVER_URL = process.env.EXPO_PUBLIC_BACKEND_SERVER_URL
 const SENTRY_DSN = process.env.EXPO_PUBLIC_SENTRY_DSN
-const ROLLBAR_ACCESS_TOKEN = process.env.EXPO_PUBLIC_ROLLBAR_ACCESS_TOKEN
 
-const { backendServerUrl, rollbarAccessToken, sentryDSN } = Constants?.expoConfig?.extra
+const { backendServerUrl, sentryDSN } = Constants?.expoConfig?.extra
 
 const isExpoGo = Constants.executionEnvironment === ExecutionEnvironment.StoreClient
 const isAndroid = Platform.OS === 'android'
@@ -16,19 +15,16 @@ const isAndroid = Platform.OS === 'android'
 const config = {
   'expo-go':{
     backendServerUrl: BACKEND_SERVER_URL,
-    rollbarAccessToken: isAndroid ? null : ROLLBAR_ACCESS_TOKEN,
     sentryDSN: SENTRY_DSN,
   
   },
   'expo_build':{
     'staging':{
-      backendServerUrl: backendServerUrl || BACKEND_SERVER_URL || 'https://sample_app-staging.herokuapp.com',
-      rollbarAccessToken: isAndroid ? null : rollbarAccessToken || ROLLBAR_ACCESS_TOKEN || "<REPLACE_WITH_STAGING_ROLLBAR_TOKEN>",
+      backendServerUrl: backendServerUrl || BACKEND_SERVER_URL || 'https://{{ cookiecutter.project_slug }}-staging.herokuapp.com',
       sentryDSN: sentryDSN || SENTRY_DSN || "<REPLACE_WITH_STAGING_SENTRY_DSN>",
     },
     'production':{
-      backendServerUrl: backendServerUrl || BACKEND_SERVER_URL || 'https://sample_app-staging.herokuapp.com',
-      rollbarAccessToken: isAndroid ? null : rollbarAccessToken || ROLLBAR_ACCESS_TOKEN || "<REPLACE_WITH_PROD_ROLLBAR_TOKEN>",
+      backendServerUrl: backendServerUrl || BACKEND_SERVER_URL || 'https://{{ cookiecutter.project_slug }}-staging.herokuapp.com',
       sentryDSN: sentryDSN || SENTRY_DSN || "<REPLACE_WITH_PROD_SENTRY_DSN>",
     }
     
