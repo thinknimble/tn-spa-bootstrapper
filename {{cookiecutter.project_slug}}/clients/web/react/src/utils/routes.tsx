253e8348acf8bf7f54a0bf242393c2684e6fa237
Pari Work Temp <pariwork@thinknimble.com>
2026-01-24 14:54:18 -0500

    feat(web): add email verification landing page
    - Add verify-email page that auto-verifies on load
    - Add verifyEmail API call to user service
    - Add /verify-email/:userId/:token route

--- {{cookiecutter.project_slug}}/clients/web/react/src/utils/routes.tsx
+++ {{cookiecutter.project_slug}}/clients/web/react/src/utils/routes.tsx
@@ -5,6 +5,7 @@ import { ChatDemo } from 'src/pages/chat-demo'
 import { PageNotFound } from 'src/pages/page-not-found'
 import { RequestPasswordReset } from 'src/pages/request-password-reset'
 import { ResetPassword } from 'src/pages/reset-password'
+import { VerifyEmail } from 'src/pages/verify-email'
 import { useAuth } from 'src/stores/auth'
 
 const PrivateRoutes = () => {
@@ -23,6 +24,7 @@ const AuthRoutes = () => {
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/request-reset" element={<RequestPasswordReset />} />
       <Route path="/password/reset/confirm/:userId/:token" element={<ResetPassword />} />
+      <Route path="/verify-email/:userId/:token" element={<VerifyEmail />} />
     </>
   )
 }
