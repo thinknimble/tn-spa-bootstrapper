6cb56ea8e60d7fc1d8880aba8629dc3dee575821
William Huster <william@thinknimble.com>
2026-07-26 12:17:34 -0600

    Fix getErrorMessages missing DRF {detail: string} error shape

--- {{cookiecutter.project_slug}}/clients/web/react/src/utils/errors.ts
+++ {{cookiecutter.project_slug}}/clients/web/react/src/utils/errors.ts
@@ -1,19 +1,19 @@
 import { isAxiosError } from 'axios'
 
-type ErrorData = string[] | { [key: string]: string[] }
+type ErrorData = string[] | { [key: string]: string[] | string }
 
 function extractErrorMessages(data: ErrorData | undefined): string[] {
   if (Array.isArray(data) && data.length && typeof data[0] === 'string') {
     return data
-  } else if (
-    typeof data === 'object' &&
-    Object.keys(data).every((key) => Array.isArray((data as { [key: string]: string[] })[key]))
-  ) {
-    // Use type assertion within every callback to access property with key
-    return Object.values(data).flat()
-  } else {
-    return ['Something went wrong']
+  } else if (typeof data === 'object' && data !== null) {
+    if ('detail' in data && typeof data['detail'] === 'string') {
+      return [data['detail']]
+    }
+    if (Object.values(data).every((v) => Array.isArray(v))) {
+      return Object.values(data as { [key: string]: string[] }).flat()
+    }
   }
+  return ['Something went wrong']
 }
 
 export function getErrorMessages(e: Error, defaultMessage = 'Something went wrong'): string[] {
